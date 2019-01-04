72c62d31e289600ac6b69f2431c316647f760f16
Nick Poole <Nick.Poole@SparkFun.com>
2019-01-03 23:08:48 -0700

    Add several features
    It's twitching but it's not working yet

--- contentScript.js
+++ contentScript.js
@@ -27,26 +27,43 @@ function pollForUpdate(){
 // Retreive settings from chrome.sync and apply timeline style changes
 function styleTimeline(){
 	
-	console.log('style timeline');
+	chrome.storage.sync.get("tagAds", function(result){if(result.tagAds == true){tagAds();}}); 
+	chrome.storage.sync.get("collapseAds", function(result){if(result.collapseAds == true){collapseAds();}}); 
+	chrome.storage.sync.get("collapseWhoToFollow", function(result){if(result.collapseWhoToFollow == true){collapseWhoToFollow();}}); 
+	chrome.storage.sync.get("collapseTrending", function(result){if(result.collapseTrending == true){collapseTrending();}}); 
+	//chrome.storage.sync.get("collapseCWs", function(result){if(result.collapseCWs == true){$("#collapseCWs").addClass("on");}}); 
+	//chrome.storage.sync.get("minifyFeed", function(result){if(result.minifyFeed == true){$("#minifyFeed").addClass("on");}}); 
+
+	//chrome.storage.sync.get("collapseTerms", function(result){$("#collapseTerms").val(result.collapseTerms);});
+	//chrome.storage.sync.get("redactTerms", function(result){$("#redactTerms").val(result.redactTerms);});
 	
 }
 
 // Retrieve settings from chrome.sync and apply title (tab) style changes
 function styleTitle(){
 	
-	console.log('style title');
+	chrome.storage.sync.get("quietTab", function(result){if(result.quietTab == true){removePendingTitle();}}); 
+	//chrome.storage.sync.get("verboseTab", function(result){if(result.verboseTab == true){$("#verboseTab").addClass("on");}}); 
 	
 }
 
 // Apply custom style to promoted tweets
 function tagAds(){
 	
+	$(".promoted-tweet").css("opacity", "0.2");
+	$(".promoted-tweet").closest("li").css("border-bottom", "1px solid #e6ecf0");
 	
 }
 
 // Collapse promoted tweets
 function collapseAds(){
-	
+
+	$(".promoted-tweet").css("height", "55px");
+	$(".promoted-tweet").find(".u-block").css("display", "none");
+	$(".promoted-tweet").find(".AdaptiveMediaOuterContainer").css("display", "none");
+	$(".promoted-tweet").find(".PlayableMedia").css("display", "none");
+	$(".promoted-tweet").find(".stream-item-footer").css("display", "none");
+	$(".promoted-tweet").find(".context").css("display", "none");
 	
 }
 
@@ -71,12 +88,28 @@ function redactTriggers(){
 // Collapse the 'Trending' menu
 function collapseTrending(){
 	
+	$('.module.Trends').find(".flex-module-inner").hide();
+	
+	$('.module.Trends').find('.flex-module-header').click(function(evt){
+		
+		evt.stopImmediatePropagation();
+		$('.module.Trends').find(".flex-module-inner").slideToggle();
+		
+	});	
 	
 }
 
 // Collapse the 'Who To Follow' menu
 function collapseWhoToFollow(){
 	
+	$('.wtf-module').find("[data-section-id='wtf']").hide();
+	
+	$('.wtf-module').find('.flex-module-header').click(function(evt){
+		
+		evt.stopImmediatePropagation();
+		$('.wtf-module').find("[data-section-id='wtf']").slideToggle();
+		
+	});
 	
 }
 
@@ -95,7 +128,7 @@ function applyCustomStyles(){
 // Change the window title to remove pending messages alert
 function removePendingTitle(){
 	
-	
+	$("title").text("Twitter");
 }
 
 // Change the window title to add message and notification alerts
--- options.js
+++ options.js
@@ -1,23 +1,67 @@
-/*
-  let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
-    }
-  }
-  constructOptions(kButtonColors);
- */
  
+ loadSettings();
+
  $('#github-button').click(function(){
 	 
 	window.open('https://github.com/NPoole/Birdcage','_blank');
 	 
  });
+ 
+ $(".switch").click(function(){
+	
+	if($(this).hasClass("on")){
+		$(this).removeClass("on");
+	}else{
+		$(this).addClass("on");
+	}
+	
+	saveSettings();
+	 
+ });
+ 
+ $("#collapseTerms").on('input',function(e){
+	 
+	 console.log('collapse terms input');
+	 saveSettings();
+
+ });
+
+ $("#redactTerms").on('input',function(e){
+	 
+	 console.log('redact terms input');
+	 saveSettings();
+
+ });
+ 
+ function saveSettings(){ 
+
+	chrome.storage.sync.set(
+		{"tagAds": $("#tagAds").hasClass("on"), 
+		"collapseAds": $("#collapseAds").hasClass("on"), 
+		"quietTab": $("#quietTab").hasClass("on"), 
+		"verboseTab": $("#verboseTab").hasClass("on"), 
+		"collapseWhoToFollow": $("#collapseWhoToFollow").hasClass("on"), 
+		"collapseTrending": $("#collapseTrending").hasClass("on"), 
+		"collapseCWs": $("#collapseCWs").hasClass("on"), 
+		"minifyFeed": $("#minifyFeed").hasClass("on"),
+		"collapseTerms": $("#collapseTerms").val(),
+		"redactTerms": $("#redactTerms").val()}
+	);
+	 
+ }
+ 
+ function loadSettings(){
+	 
+	chrome.storage.sync.get("tagAds", function(result){if(result.tagAds == true){$("#tagAds").addClass("on");}}); 
+	chrome.storage.sync.get("collapseAds", function(result){if(result.collapseAds == true){$("#collapseAds").addClass("on");}}); 
+	chrome.storage.sync.get("quietTab", function(result){if(result.quietTab == true){$("#quietTab").addClass("on");}}); 
+	chrome.storage.sync.get("verboseTab", function(result){if(result.verboseTab == true){$("#verboseTab").addClass("on");}}); 
+	chrome.storage.sync.get("collapseWhoToFollow", function(result){if(result.collapseWhoToFollow == true){$("#collapseWhoToFollow").addClass("on");}}); 
+	chrome.storage.sync.get("collapseTrending", function(result){if(result.collapseTrending == true){$("#collapseTrending").addClass("on");}}); 
+	chrome.storage.sync.get("collapseCWs", function(result){if(result.collapseCWs == true){$("#collapseCWs").addClass("on");}}); 
+	chrome.storage.sync.get("minifyFeed", function(result){if(result.minifyFeed == true){$("#minifyFeed").addClass("on");}}); 
+
+	chrome.storage.sync.get("collapseTerms", function(result){$("#collapseTerms").val(result.collapseTerms);});
+	chrome.storage.sync.get("redactTerms", function(result){$("#redactTerms").val(result.redactTerms);});
+	 
+ }
